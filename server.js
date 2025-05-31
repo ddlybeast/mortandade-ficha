0b0bcc7ee9d64534396bd1a60a6710823d158f7e
ddlybeast <deadlystarboyy@gmail.com>
2025-05-31 14:39:56 -0300

    Commit Arrumado

--- server.js
+++ server.js
@@ -50,13 +50,4 @@ app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 
-const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log("✅ Conectado ao MongoDB Atlas");
-}).catch(err => {
-  console.error("❌ Erro ao conectar ao MongoDB:", err);
-});
